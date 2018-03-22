cd9d21338de8f7b229da246e0a36aa0cfcb52af3
md mukibul islam <mukibul@gmail.com>
2018-03-22 20:11:09 +0530

    commit feature branch

--- src/main/java/com/toppertalks/tt/gateway/GatewayStarter.java
+++ src/main/java/com/toppertalks/tt/gateway/GatewayStarter.java
@@ -1,4 +1,4 @@
-package com.skidata.ssp.gateway;
+package com.toppertalks.tt.gateway;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/toppertalks/tt/gateway/config/ResourceConfiguration.java
@@ -0,0 +1,89 @@
+package com.toppertalks.tt.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.web.header.HeaderWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author firoz
+ * @since 15/11/16
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Value("${security.jwt.resource-ids}")
+    private String resourceIds;
+
+    @Value("${security.signing-key}")
+    private String signingKey;
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(signingKey);
+        return converter;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId(resourceIds).tokenServices(tokenServices());
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**").authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers("/tt-auth/**", "/env/**", "/metrics/**").permitAll().anyRequest()
+                .authenticated();
+                http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//         http
+//                .authorizeRequests()
+//                .antMatchers( "/**").access("#oauth2.hasScope('call-services')")
+//                .and()
+//                .headers().addHeaderWriter(new HeaderWriter() {
+//            @Override
+//            public void writeHeaders(HttpServletRequest request, HttpServletResponse response) {
+//                response.addHeader("Access-Control-Allow-Origin", "*");
+//                if (request.getMethod().equals("OPTIONS")) {
+//                    response.setHeader("Access-Control-Allow-Methods", request.getHeader("Access-Control-Request-Method"));
+//                    response.setHeader("Access-Control-Allow-Headers", request.getHeader("Access-Control-Request-Headers"));
+//                }
+//            }
+//        });
+    }
+
+//    @Override
+//    public void configure(final ResourceServerSecurityConfigurer resources) {
+//        resources.resourceId("tt-gateway");
+//    }
+}
